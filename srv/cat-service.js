eb4c4c7c694bbf79c48f9c687e69782cfa1d7027
DESKTOP-C3CDDM5\e_cas <e_castella@hotmail.com>
2022-05-25 22:11:59 +0200

    Init version

---
+++ srv/cat-service.js
@@ -0,0 +1,40 @@
+const cds = require("@sap/cds")
+
+module.exports = cds.service.impl (function(){
+  this.on ('getTodos', async (req) => {
+
+    const todoApi = await cds.connect.to("SODUKU-PYTHON2")
+    return todoApi.tx(req).get("/")
+
+  }) 
+})
+
+/*
+class CatalogService extends cds.ApplicationService {
+
+  async init(){
+      this.on('getTodos', async (req) => {
+          const todoApi = await cds.connect.to("SODUKU-PYTHON2")
+          console.log("Cached!");
+          return todoApi.tx(req).get("/custom?size=2&dificulty=0.1")
+      }
+      
+      
+      )
+  }
+  
+  }
+  
+  
+module.exports = {CatalogService}
+*/
+/*
+module.exports = (srv) => {
+
+    srv.on ('READ', 'Books', async (req)=> {
+      const openWeatherApi = await cds.connect.to("SODUKU-PYTHON2");
+      return openWeatherApi.tx(req).get("/custom?size=2&dificulty=0.1");
+    });
+  
+  }
+  */
